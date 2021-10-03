5339f33f3cb8b1dad5a56f7868345c3544991a70
Ethan C <ethan.cho1826@gmail.com>
2021-10-03 00:33:04 -0500

    added search history function and local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,9 @@
+var inputHistory = [];
+
 function getApi(cityName) {
     var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=537c5082f054c67490bdd35711142b24`;
 
     var currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=537c5082f054c67490bdd35711142b24`;
-
-    $.ajax({
-        url: currentWeather,
-        method: 'GET',
-    }).then(function (response) {
-        // console.log("current weather")
-        let temp = response.main.temp;
-        let wind = response.wind.speed;
-        let humidity = response.main.humidity;
-        console.log(temp, wind, humidity);
-        $(".temp").text("Temp: " + temp + String.fromCharCode(176) + "F");
-        $(".wind").text("Wind: " + wind + " MPH");
-        $(".humidity").text("Humditiy: " + humidity + "%");
-        let dailyIconCode = response.weather[0].icon;
-        let dailyIconUrl = `http://openweathermap.org/img/wn/${dailyIconCode}@2x.png`;
-        let img = $("<img>")
-        img.attr("src", dailyIconUrl);
-        img.appendTo("#city");
-        let latitude = response.coord.lat;
-        let longitude = response.coord.lon;
-        var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=537c5082f054c67490bdd35711142b24`;
-
-        $.ajax({
-            url: oneCall,
-            method: 'GET'
-        }).then(function (res) {
-            $(".uvHeader").text("UV Index: ");
-            $("#uvIndex").text(res.current.uvi);
-            if(res.current.uvi <= 2) {
-                $("#uvIndex").attr("class", "safe");
-            } else if(res.current.uvi <= 5) {
-                $("#uvIndex").attr("class", "warning");
-            } else if(res.current.uvi <= 7) {
-                $("#uvIndex").attr("class", "orange");
-            } else {
-                $("#uvIndex").attr("class", "danger");
-            }
-        }).catch(function (error) {
-            console.log(error.responseJSON.cod, error.responseJSON.message);
-        })
-    }).catch(function (error) {
-        console.log(error.responseJSON.cod, error.responseJSON.message);
-    })
 
     $.ajax({
         url: fiveDayForecast,
@@ -74,22 +33,77 @@ function getApi(cityName) {
             $("#" + j).append(windEl);
             $("#" + j).append(humidityEl);
         }
+    }).catch(function (error) {
+        console.log(error.responseJSON.cod, error.responseJSON.message);
+    })
 
+    $.ajax({
+        url: currentWeather,
+        method: 'GET',
+    }).then(function (response) {
+        // console.log("current weather")
+        let temp = response.main.temp;
+        let wind = response.wind.speed;
+        let humidity = response.main.humidity;
+        console.log(temp, wind, humidity);
+        $(".temp").text("Temp: " + temp + String.fromCharCode(176) + "F");
+        $(".wind").text("Wind: " + wind + " MPH");
+        $(".humidity").text("Humditiy: " + humidity + "%");
+        let dailyIconCode = response.weather[0].icon;
+        let dailyIconUrl = `http://openweathermap.org/img/wn/${dailyIconCode}@2x.png`;
+        let img = $("<img>")
+        img.attr("src", dailyIconUrl);
+        
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=537c5082f054c67490bdd35711142b24`;
+
+        $.ajax({
+            url: oneCall,
+            method: 'GET'
+        }).then(function (res) {
+            $(".uvHeader").text("UV Index: ");
+            $("#uvIndex").text(res.current.uvi);
+            if(res.current.uvi <= 2) {
+                $("#uvIndex").attr("class", "safe");
+            } else if(res.current.uvi <= 5) {
+                $("#uvIndex").attr("class", "warning");
+            } else if(res.current.uvi <= 7) {
+                $("#uvIndex").attr("class", "orange");
+            } else {
+                $("#uvIndex").attr("class", "danger");
+            }
+
+            img.appendTo("#city");
+        }).catch(function (error) {
+            console.log(error.responseJSON.cod, error.responseJSON.message);
+        })
     }).catch(function (error) {
         console.log(error.responseJSON.cod, error.responseJSON.message);
     })
 }
 
-function searchHandler(event) {
+function historyHandler(event) {
     event.preventDefault();
-    let searchInput = $(".searchBody").val();
-    // console.log(searchInput);
+    console.log($(this));
+    let searchInput = $(this).text();
+    console.log(searchInput);
     clearCard();
     getApi(searchInput);
 }
 
-function storeHistory(string) {
-
+    
+function searchHandler(event) {
+    event.preventDefault();
+    console.log("fired");
+    let searchInput = $(".searchInput").val();
+    console.log(searchInput);
+    storeHistory(searchInput);
+    
+    clearCard();
+    getApi(searchInput);
+    getHistory();
+    $(".historyBtn").on("click", historyHandler);
 }
 
 function clearCard() {
@@ -97,7 +111,33 @@ function clearCard() {
     $(".futureIcon").remove();
 }
 
-$(".search").on("click", ".searchButton", searchHandler);
+function storeHistory(input) {
+    console.log("storing now!")
+    inputHistory.push(input);
+    localStorage.setItem("input", JSON.stringify(inputHistory));
+}
+
+function getHistory() {
+    $(".historyBtn").remove();
+    inputHistory = JSON.parse(localStorage.getItem("input"));
+    for (let i = 0; i < inputHistory.length; i++) {
+        let historyBtn = $("<button>");
+        historyBtn.attr("class", "historyBtn");
+        historyBtn.addClass("btn button btn-primary");
+        historyBtn.text(inputHistory[i]);
+        $(".searchHistory").append(historyBtn);
+    }
+}
+
+$(".button").on("click", searchHandler);
+
+
 // console.log($(".searchButton"));
 
-// bg-success (green), bg-warning (yellow), bg-danger (red), bg-orange
+// local storage function:
+// store values into array then put array into local storage
+// sets the search history into local storage at different key values 
+
+// local storage get function:
+// gets array stored in local storage
+// iterate over array and generate buttons with values slapped on them
