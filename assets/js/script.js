f31c009937daccbd0042c840cd82eda4eab3f244
Ethan C <ethan.cho1826@gmail.com>
2021-10-02 23:02:07 -0500

    fixed bugs for html elements being generated dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,7 @@ function getApi(cityName) {
         url: currentWeather,
         method: 'GET',
     }).then(function (response) {
-        // console.log(response);
-        console.log("current weather")
-        // console.log(response.coord.lat);
-        // console.log(response.coord.lon);
-        let currentDate = moment().format("M/DD/YYYY")
-        $("#city").text(cityName + " " + currentDate);
+        // console.log("current weather")
         let temp = response.main.temp;
         let wind = response.wind.speed;
         let humidity = response.main.humidity;
@@ -21,30 +16,33 @@ function getApi(cityName) {
         $(".wind").text("Wind: " + wind + " MPH");
         $(".humidity").text("Humditiy: " + humidity + "%");
         let dailyIconCode = response.weather[0].icon;
-        // console.log(dailyIconCode);
         let dailyIconUrl = `http://openweathermap.org/img/wn/${dailyIconCode}@2x.png`;
         let img = $("<img>")
         img.attr("src", dailyIconUrl);
         img.appendTo("#city");
         let latitude = response.coord.lat;
         let longitude = response.coord.lon;
-        // console.log(latitude);
-        // console.log(longitude);
         var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=537c5082f054c67490bdd35711142b24`;
 
         $.ajax({
             url: oneCall,
             method: 'GET'
         }).then(function (res) {
-            // console.log(res.current.uvi);
             $(".uvHeader").text("UV Index: ");
-            $(".uvIndex").text(res.current.uvi);
+            $("#uvIndex").text(res.current.uvi);
+            if(res.current.uvi <= 2) {
+                $("#uvIndex").attr("class", "safe");
+            } else if(res.current.uvi <= 5) {
+                $("#uvIndex").attr("class", "warning");
+            } else if(res.current.uvi <= 7) {
+                $("#uvIndex").attr("class", "orange");
+            } else {
+                $("#uvIndex").attr("class", "danger");
+            }
         }).catch(function (error) {
-            // console.log(error);
             console.log(error.responseJSON.cod, error.responseJSON.message);
         })
     }).catch(function (error) {
-        // console.log(error);
         console.log(error.responseJSON.cod, error.responseJSON.message);
     })
 
@@ -52,17 +50,21 @@ function getApi(cityName) {
         url: fiveDayForecast,
         method: 'GET'
     }).then(function (response) {
+        let currentDate = moment().format("M/DD/YYYY");
+        $("#city").text(response.city.name + " " + currentDate);
         for (i = 5; i < response.list.length; i += 8) { // contribution Sue Lee
             let j = i.toString();
             let dateEl = $("<li>");
             dateEl.text(moment(response.list[i].dt_txt).format('M/D/YYYY')); // contribution Sue Lee
+            dateEl.attr("class", "bolder");
             let tempEl = $("<li>");
-            tempEl.text("Temp: " + response.list[i].main.temp + String.fromCharCode(176) + "F");
+            tempEl.text("Temp: " + response.list[i].main.temp + String.fromCharCode(176) + "F"); 
             let windEl = $("<li>");
             windEl.text("Wind: " + response.list[i].wind.speed + " MPH");
             let humidityEl = $("<li>");
             humidityEl.text("Humidity: " + response.list[i].main.humidity + "%");
             let iconEl = $("<img>");
+            iconEl.attr("class", "futureIcon");
             let iconCode = response.list[i].weather[0].icon;
             let iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
             iconEl.attr("src", iconUrl);
@@ -80,14 +82,22 @@ function getApi(cityName) {
 
 function searchHandler(event) {
     event.preventDefault();
-    let searchInput = capitalize($(".searchBody").val());
-    console.log(searchInput);
+    let searchInput = $(".searchBody").val();
+    // console.log(searchInput);
+    clearCard();
     getApi(searchInput);
 }
 
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function storeHistory(string) {
+
 }
 
-$(".searchButton").on("click", searchHandler);
-console.log($(".searchButton"));
+function clearCard() {
+    $("li").remove();
+    $(".futureIcon").remove();
+}
+
+$(".search").on("click", ".searchButton", searchHandler);
+// console.log($(".searchButton"));
+
+// bg-success (green), bg-warning (yellow), bg-danger (red), bg-orange
